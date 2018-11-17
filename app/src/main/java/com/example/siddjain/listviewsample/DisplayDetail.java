5637c728265e55285dd91c9f310f4bde28dc26ca
Siddharth Kumar Jain <s8002.sid@gmail.com>
2018-11-17 10:49:21 +0530

    Starting new activity on item click and sending id corresponding to item clicked.
    Starting new activity on item click and sending id corresponding to item clicked.

--- app/src/main/java/com/example/siddjain/listviewsample/DisplayDetail.java
+++ app/src/main/java/com/example/siddjain/listviewsample/DisplayDetail.java
@@ -1,5 +1,6 @@
 package com.example.siddjain.listviewsample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class DisplayDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_detail);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.MESSAGE_ID);
+
     }
 }
--- app/src/main/java/com/example/siddjain/listviewsample/MainActivity.java
+++ app/src/main/java/com/example/siddjain/listviewsample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.siddjain.listviewsample;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
@@ -8,20 +9,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList<String> fID;
+    public static final String MESSAGE_ID = "com.example.siddjain.listviewsample.MESSAGE_ID";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fID = new ArrayList<>();
         DataBaseHelper myDbHelper = new DataBaseHelper(this);
 
         try {
@@ -48,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         db = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
         Cursor resultSet;
         try {
-            resultSet = db.rawQuery("Select name, phone_number from family_master", null);
+            resultSet = db.rawQuery("Select name, phone_number, _id from family_master order by name", null);
         ArrayList<String> nameList = new ArrayList<>();
             ArrayList<String> phoneNumberList = new ArrayList<>();
         resultSet.moveToFirst();
@@ -56,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
         {
             nameList.add(resultSet.getString(0));
             phoneNumberList.add(resultSet.getString(1));
+            Integer val = new Integer(resultSet.getInt(2));
+            fID.add(Integer.toString(val));
             resultSet.moveToNext();
         }
 
@@ -68,7 +71,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // Get the selected item text from ListView
-                String selectedItem = (String) parent.getItemAtPosition(position);
+                //String selectedItem = (String) parent.getItemAtPosition(position);
+                String rowid = fID.get(position);
+                Intent intent = new Intent(view.getContext(), DisplayDetail.class);
+                intent.putExtra(MESSAGE_ID, rowid);
+                startActivity(intent);
             }
         });
 
